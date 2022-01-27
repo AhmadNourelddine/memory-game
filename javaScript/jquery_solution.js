f5555e2fc43a84944a49eca4626f899e4a9e9118
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-01-27 21:38:17 +0200

    fixed button click not stopping problem

--- javaScript/jquery_solution.js
+++ javaScript/jquery_solution.js
@@ -1,9 +1,9 @@
+$(document).ready(function() {
 
-$(document).ready( function(){
-
-  $(document).keydown( function(){
+  $(document).keydown(function() {
 
     var level = 0
+    var clicks = 0
     const colors = ['green', 'red', 'yellow', 'blue']
     let saved_colors = []
 
@@ -11,71 +11,86 @@ $(document).ready( function(){
 
     function startGame() {
       console.log('game started')
+
       level = 0
+      saved_colors = []
       $('#title').text('Press Any Key to Start')
       generateLevel()
+
     }
 
     function generateLevel() {
       level += 1
-      console.log('generate level '+level)
-      $('#title').text('level '+level)
+      clicks = 0
+
+      console.log('generating level ' + level)
+
+      $('#title').text('level ' + level)
+
       var x = Math.floor(Math.random() * 4)
       var color = colors[x]
-      console.log('color is '+colors[x])
+      console.log('random color is ' + colors[x])
       saved_colors.push(color)
 
       src = 'sounds/' + color + '.mp3'
       var audio = new Audio(src)
       audio.play()
 
-      console.log(`.btn, .${color}`)
+      console.log(saved_colors)
+
       $(`#${color}`).fadeOut('slow')
-      setTimeout( ()=>{
+      setTimeout(() => {
         $(`#${color}`).fadeIn('slow')
-      },500)
+      }, 500)
+
       check_clicks()
     }
 
     function check_clicks() {
 
-      var clicks = 0
-
       $('.btn').click(function(event) {
-        console.log(event.target.id)
+
+        console.log('button clicked ' + event.target.id)
+
         src = 'sounds/' + event.target.id + '.mp3'
         var audio = new Audio(src)
         audio.play()
 
         $(event.target).addClass('pressed')
-        setTimeout(()=>{
-        $(event.target).removeClass('pressed')
-        },500)
-        console.log($(event.target).hasClass(saved_colors[clicks]))
+        setTimeout(() => {
+          $(event.target).removeClass('pressed')
+        }, 500)
+
         if ($(event.target).hasClass(saved_colors[clicks]) != true) {
+          console.log('lost game')
+          $('.btn').off('click')
           lose_game()
           return
         }
-        clicks ++
+        clicks++
+        console.log('increment clicks: ' + clicks)
         if (clicks == level) {
+          setTimeout(() => {
+            $('.btn').off('click')
             generateLevel()
-          return
+          }, 1500)
         }
-        return
-
       })
-      return
     }
 
     function lose_game() {
-      $('title').text('Game Over, Press Any Key to Restart')
+
+      $('#title').text('Game Over, Press Any Key to Restart')
+
       src = 'sounds/wrong.mp3'
       var audio = new Audio(src)
       audio.play()
+
       document.body.classList.add('game-over')
-      setTimeout(()=>{
+      setTimeout(() => {
         document.body.classList.remove('game-over')
-      },1000)
+      }, 1000)
     }
+
   })
 })
