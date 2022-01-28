4f11c7e2c1def18084ee107aee116228e55f0610
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-01-28 20:25:53 +0200

    problem solved in keydown called many times

--- javaScript/jquery_solution.js
+++ javaScript/jquery_solution.js
@@ -1,96 +1,101 @@
 $(document).ready(function() {
 
   $(document).keydown(function() {
-
-    var level = 0
-    var clicks = 0
-    const colors = ['green', 'red', 'yellow', 'blue']
-    let saved_colors = []
-
     startGame()
+  })
 
-    function startGame() {
-      console.log('game started')
+  var level = 0
+  var clicks = 0
+  const colors = ['green', 'red', 'yellow', 'blue']
+  let saved_colors = []
 
-      level = 0
-      saved_colors = []
-      $('#title').text('Press Any Key to Start')
-      generateLevel()
 
-    }
+  function startGame() {
+    console.log('game started')
+    $(document).off('keydown')
 
-    function generateLevel() {
-      level += 1
-      clicks = 0
+    level = 0
+    saved_colors = []
+    $('#title').text('Press Any Key to Start')
+    generateLevel()
 
-      console.log('generating level ' + level)
+  }
 
-      $('#title').text('level ' + level)
+  function generateLevel() {
+    level += 1
+    clicks = 0
 
-      var x = Math.floor(Math.random() * 4)
-      var color = colors[x]
-      console.log('random color is ' + colors[x])
-      saved_colors.push(color)
+    console.log('generating level ' + level)
 
-      src = 'sounds/' + color + '.mp3'
+    $('#title').text('level ' + level)
+
+    var x = Math.floor(Math.random() * 4)
+    var color = colors[x]
+    console.log('random color is ' + colors[x])
+    saved_colors.push(color)
+
+    src = 'sounds/' + color + '.mp3'
+    var audio = new Audio(src)
+    audio.play()
+
+    console.log(saved_colors)
+
+    $(`#${color}`).fadeOut('slow')
+    setTimeout(() => {
+      $(`#${color}`).fadeIn('slow')
+    }, 500)
+
+    check_clicks()
+  }
+
+  function check_clicks() {
+
+    $('.btn').click(function(event) {
+
+      console.log('button clicked ' + event.target.id)
+
+      src = 'sounds/' + event.target.id + '.mp3'
       var audio = new Audio(src)
       audio.play()
 
-      console.log(saved_colors)
-
-      $(`#${color}`).fadeOut('slow')
+      $(event.target).addClass('pressed')
       setTimeout(() => {
-        $(`#${color}`).fadeIn('slow')
+        $(event.target).removeClass('pressed')
       }, 500)
 
-      check_clicks()
-    }
-
-    function check_clicks() {
-
-      $('.btn').click(function(event) {
-
-        console.log('button clicked ' + event.target.id)
-
-        src = 'sounds/' + event.target.id + '.mp3'
-        var audio = new Audio(src)
-        audio.play()
-
-        $(event.target).addClass('pressed')
+      if ($(event.target).hasClass(saved_colors[clicks]) != true) {
+        console.log('lost game')
+        $('.btn').off('click')
+        lose_game()
+        return
+      }
+      clicks++
+      console.log('increment clicks: ' + clicks)
+      if (clicks == level) {
         setTimeout(() => {
-          $(event.target).removeClass('pressed')
-        }, 500)
-
-        if ($(event.target).hasClass(saved_colors[clicks]) != true) {
-          console.log('lost game')
           $('.btn').off('click')
-          lose_game()
-          return
-        }
-        clicks++
-        console.log('increment clicks: ' + clicks)
-        if (clicks == level) {
-          setTimeout(() => {
-            $('.btn').off('click')
-            generateLevel()
-          }, 1500)
-        }
-      })
-    }
+          generateLevel()
+        }, 1500)
+      }
+    })
+  }
 
-    function lose_game() {
+  function lose_game() {
 
-      $('#title').text('Game Over, Press Any Key to Restart')
+    $('#title').text('Game Over, Press Any Key to Restart')
 
-      src = 'sounds/wrong.mp3'
-      var audio = new Audio(src)
-      audio.play()
+    src = 'sounds/wrong.mp3'
+    var audio = new Audio(src)
+    audio.play()
 
-      document.body.classList.add('game-over')
-      setTimeout(() => {
-        document.body.classList.remove('game-over')
-      }, 1000)
-    }
+    document.body.classList.add('game-over')
+    setTimeout(() => {
+      document.body.classList.remove('game-over')
+    }, 1000)
 
-  })
+    $(document).keydown(function() {
+      startGame()
+    })
+  }
+
 })
